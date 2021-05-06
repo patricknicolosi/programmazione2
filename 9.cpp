38cd87a42bb91d032722d69725d723d65cff7759
patricknicolosi <patricknicolosi99@gmail.com>
2021-05-06 19:00:36 +0200

    Completata gerarchia classe Veicoli. Da completare class Gara

--- 9.cpp
+++ 9.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 using namespace std;
 
 /*
 Implementare una classe che rappresenti una gerarchia di veicoli
 Ogni veicolo ha un numero di ruote, un tipo di carburante, una velocità massima,
 una marca ed un prezzo. 
-Tra i veicoli distinguiamo motocicli, automobili, camion, mezzi speciali.
+Tra i veicoli distinguiamo motocicli e automobili.
 Si simuli una gara tra 10 veicoli di tipo diverso, riportando posizioni iniziali, sorpassi e classifica finale 
 */
 
@@ -86,15 +87,15 @@ class Veicolo{
         }
 };
 
-class Moto : public Veicolo{
+class Motociclo : public Veicolo{
     public: 
-        Moto(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione) : 
+        Motociclo(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione) : 
         Veicolo (velocitaMax, _prezzo, _marca, _motorizzazione, "benzina", 2){
             velocitaMax = _velocitaMax;
         }
         string toString() const{
             stringstream stream;
-            stream << "Moto: " << endl;
+            stream << "Motociclo: " << endl;
             stream << "-Velocita massima: " << velocitaMax << endl;
             stream << "-Prezzo: " << prezzo << endl;
             stream << "-Marca: " << marca << endl;
@@ -121,50 +122,69 @@ class Automobile : public Veicolo{
         }    
 };
 
-class Camion : public Veicolo{
-    public: 
-        Camion(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione, string _carburante, int _numeroRuote) : 
-        Veicolo (velocitaMax, _prezzo, _marca, _motorizzazione, _carburante, _numeroRuote){
-            velocitaMax = _velocitaMax;
-        }
-        string toString() const{
-            stringstream stream;
-            stream << "Camion: " << endl;
-            stream << "-Velocita massima: " << velocitaMax << endl;
-            stream << "-Prezzo: " << prezzo << endl;
-            stream << "-Marca: " << marca << endl;
-            stream << "-Motorizzazione: " << motorizzazione << endl;
-            stream << "-Carburante: " << carburante << endl;
-            stream << "-Numero ruote: " << numeroRuote << endl;
-            return stream.str();
-        }    
-};
 
-class Gara{
+/*class Gara{
+    Veicolo *veicoli;
+    int numeroPartecipanti;
+    int durata;
+    void controlla();
+
     public:
+        Gara(int _durata, int _numeroPartecipanti){
+            this->durata = _durata;
+            this->numeroPartecipanti = _numeroPartecipanti;
+            veicoli = new Veicolo[numeroPartecipanti];
+        }
+        void partenza(){
+            srand(42);
+            for(int i=0; i<durata; i++){
+                for(int j=0; j<numeroPartecipanti; j++){
+                    int scelta = rand()%2;
+                    if(scelta==1)
+                        veicoli[j].accelera();
+                    else
+                        veicoli[j].decelera();
+                    aggiorna();
+                }
+                controlla();
+            }
+            stampaClassifica();
+        }
+        void aggiorna(){
+            return;
+        }
+        void controlla(){
+            return;
+        }
+        void stampaClassifica(){
+            cout << "[ ";
+            for(int i=0; i<numeroPartecipanti; i++) cout << veicoli[i].getMarca() << ", ";
+            cout << " ]" << endl;
+            return;
+        }
         void sorpassa(Veicolo& veicoloSorpassante, Veicolo& veicoloSorpassato){
             if(veicoloSorpassante.velocita > veicoloSorpassato.velocita){
                 veicoloSorpassante.posizione++;
                 veicoloSorpassato.posizione--;
                 cout << "Il primo veicolo ha superato il secondo" << endl;
+                return;   
             }
             else{
                 veicoloSorpassante.posizione--;
                 veicoloSorpassato.posizione++;
                 cout << "Il secondo veicolo ha superato il primo" << endl;
-            }   
+                return;
+            }
         }
-};
+        ~Gara(){ //Distruttore per svuotare la memoria dopo l'instaziamento di un oggeto
+            delete [] veicoli; //Fortemente consigliato in questi casi (obbligatorio per il prof Moltisanti)
+        }
+};*/
 
 int main(){
-    Moto moto = Moto(30, 100.50, "Ducati", "1000cc");
-    cout << moto << endl;
-
+    Motociclo motociclo = Motociclo(30, 100.50, "Ducati", "1000cc");
+    cout << motociclo << endl;
     Automobile automobile = Automobile(30, 100.50, "Scania", "1000cc", "Diesel");
     cout << automobile << endl;
-
-    Camion camion = Camion(30, 100.50, "Fiat Punto", "1000cc", "Diesel", 6);
-    cout << camion << endl;
-
     return 0;
 }
