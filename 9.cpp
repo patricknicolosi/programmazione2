7957f2cf99a6df0ebb0b6b3b1fbc456d4c9dd359
patricknicolosi <patricknicolosi99@gmail.com>
2021-05-06 17:56:00 +0200

    Esercizio gerarchia classi Veicoli e gara tra questi veicoli

--- 9.cpp
+++ 9.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 /*
@@ -22,6 +23,8 @@ class Veicolo{
     public:
         int velocita; //Velocità istantanea
         int posizione; //La posizione attuale del veicolo
+        Veicolo(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione, string _carburante, int _numeroRuote):
+        velocitaMax(_velocitaMax), prezzo(_prezzo), marca(_marca), motorizzazione(_motorizzazione), carburante(_carburante), numeroRuote(_numeroRuote){}
         int getNumeroRuote(){
             return numeroRuote;
         }
@@ -65,22 +68,76 @@ class Veicolo{
             return;
         }
         void accelera(int _velocita){
-            this->velocita += _velocita;
+            if(velocita <= velocitaMax){
+                this->velocita++;
+            }
             return;
         }
         void decelera(int _velocita){
-            int supp = this->velocita - _velocita;
-            if(supp<0){
-                cout << "Il veicolo non può decelerrare pi di cosi" << endl;
-            }
-            else{
-                this->velocita -= _velocita;
+            if(velocita > 0){
+                this->velocita--;
             }
             return;
         }
-        string toString(){
-            return "ciao mondo";
+        virtual string toString() const = 0; //virtual perchè non puo essere richiamata da un'istanza di Veicolo ma da una sua sottoclasse
+        friend ostream& operator<<(ostream& stream, const Veicolo& veicolo){
+            stream << veicolo.toString(); //Per accedere a questa funzione dobbiamo dichiare const la funzione toString
+            return stream;
         }
+};
+
+class Moto : public Veicolo{
+    public: 
+        Moto(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione) : 
+        Veicolo (velocitaMax, _prezzo, _marca, _motorizzazione, "benzina", 2){
+            velocitaMax = _velocitaMax;
+        }
+        string toString() const{
+            stringstream stream;
+            stream << "Moto: " << endl;
+            stream << "-Velocita massima: " << velocitaMax << endl;
+            stream << "-Prezzo: " << prezzo << endl;
+            stream << "-Marca: " << marca << endl;
+            stream << "-Motorizzazione: " << motorizzazione << endl;
+            return stream.str();
+        }
+};
+
+class Automobile : public Veicolo{
+    public: 
+        Automobile(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione, string _carburante) : 
+        Veicolo (velocitaMax, _prezzo, _marca, _motorizzazione, _carburante, 4){
+            velocitaMax = _velocitaMax;
+        }
+        string toString() const{
+            stringstream stream;
+            stream << "Automobile: " << endl;
+            stream << "-Velocita massima: " << velocitaMax << endl;
+            stream << "-Prezzo: " << prezzo << endl;
+            stream << "-Marca: " << marca << endl;
+            stream << "-Motorizzazione: " << motorizzazione << endl;
+            stream << "-Carburante: " << carburante << endl;
+            return stream.str();
+        }    
+};
+
+class Camion : public Veicolo{
+    public: 
+        Camion(int _velocitaMax, double _prezzo, string _marca, string _motorizzazione, string _carburante, int _numeroRuote) : 
+        Veicolo (velocitaMax, _prezzo, _marca, _motorizzazione, _carburante, _numeroRuote){
+            velocitaMax = _velocitaMax;
+        }
+        string toString() const{
+            stringstream stream;
+            stream << "Camion: " << endl;
+            stream << "-Velocita massima: " << velocitaMax << endl;
+            stream << "-Prezzo: " << prezzo << endl;
+            stream << "-Marca: " << marca << endl;
+            stream << "-Motorizzazione: " << motorizzazione << endl;
+            stream << "-Carburante: " << carburante << endl;
+            stream << "-Numero ruote: " << numeroRuote << endl;
+            return stream.str();
+        }    
 };
 
 class Gara{
@@ -100,7 +157,14 @@ class Gara{
 };
 
 int main(){
-    Veicolo v;
-    cout <<  v.getMotorizzazione() << endl;
+    Moto moto = Moto(30, 100.50, "Ducati", "1000cc");
+    cout << moto << endl;
+
+    Automobile automobile = Automobile(30, 100.50, "Scania", "1000cc", "Diesel");
+    cout << automobile << endl;
+
+    Camion camion = Camion(30, 100.50, "Fiat Punto", "1000cc", "Diesel", 6);
+    cout << camion << endl;
+
     return 0;
 }
