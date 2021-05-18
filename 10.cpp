9a754a1da72ba84d9d268148806964c5236cf7d1
Patrick Nicolosi <patricknicolosi99@gmail.com>
2021-05-18 15:25:00 +0200

    Update 10.cpp
    Fix del costruttore Node per funzionare con la classe Studente

--- 10.cpp
+++ 10.cpp
@@ -1,107 +1,99 @@
-#include<iostream>
-#include <sstream>
+#include <iostream>
 using namespace std;
+
+
+class Studente{
+    string nome;
+    string cognome;
+    
+    public:
+        Studente(string nome, string cognome){
+            this->nome = nome;
+            this->cognome = cognome;
+        }
+        friend ostream& operator<< (ostream& stream, const Studente& studente){
+            stream << studente.nome << ", " << studente.cognome;
+            return stream;
+        }
+};
+
 
 template <class T>
 class Node{
-	T value; //contenuto di ogni nodo
-	Node<T>* next; //puntatore al prossimo nodo
-
-	public:
-		Node(T value, Node<T>* next) : value(value), next(next){}
-		Node(T value) : Node(value, NULL){}
-		Node() : Node(0, NULL){}
-
-		Node<T>* getNext(){ 
-			return this->next;
-		}
-		T getValue(){
-			return this->value;
-		}
-		void setNext(Node<T>* next){
-			this->next = next;
-			return;
-		}
-		void setValue(T value){
-			this->value = value;
-		}
-		string toString(){
-			stringstream ss;
-			ss << "Node value = " << value << ", next = " << next;
-			return ss.str();
-		}
-		friend ostream& operator<<(ostream& stream, Node<T> node){
-			stream << "Node value = " << node.getValue() << ", next = " << node.getNext();
-			return stream;
-		}
+    Node* next;
+    T value;
+    
+    public:
+        Node(T value, Node<T> * next) : value(value), next(next) {}
+        Node(T value) : Node(value, NULL) {}
+        
+        T getValue() const {
+            return this->value;
+        }
+        Node* getNext() const{
+            return this->next;
+        }
+        void setNext(Node* newNode){
+            this->next = newNode;
+            return;
+        }
+        friend ostream& operator<< (ostream& stream, const Node& node){
+            stream << "Prossimo nodo =  " << node.next << ", di valore = " << node.getValue();
+            return stream;
+        }
 };
+
 
 template <class T>
 class List{
-	Node<T>* head;
-	int length;
-
-	public:
-		List(Node<T>* head) : head(head){}
-		List() : List(NULL){}
-
-		void add(T value){ //Metodo alternativo
-			Node<T>* newNode = new Node<T>(value); //da fare cosi per una questione di ciclo di vita nello scope
-			add(newNode);
-			return;
-		}
-		void add(Node<T>* newNode){
-			//Per inserire in coda bisogna scorrere tutta la lista e andare ad inserire alla fine il nodo
-			if(head == NULL){ //Se la lista è vuota allora head è uguale al nodo che voglio inserire
-				head = newNode;
-				length++;
-				return;
-			}
-			else{
-				Node<T>* current = head;
-				while(current->getNext() != NULL){ 
-					current = current->getNext();
-				}
-				//una volta finito il cilco mi trovo sull'ultimo nodo, posso quindi assegnare il nuovo valore
-				current->setNext(newNode);
-				length++;
-				return;
-			}
-		}
-		/*void remove(){
-
-		}*/
-		bool search(T value){
-			Node<T>* current = head;
-			while(current->getNext() != NULL){ 
-				if(current->getValue() == value) return true;
-				current = current->getNext();
-			}
-			return false;
-		}
-		friend ostream& operator<<(ostream& stream, const List<T>& list){
-			Node<T>* current = list.head;
-			while(current != NULL){
-				stream << *current << endl;
-				current = current->getNext();
-			}
-			return stream;
-		}
+    Node<T>* head;
+    int length;
+    
+    public:
+        List(){
+            this->head = NULL;
+            this->length = 0;
+        }
+        void add(T value){            
+            Node<T> * temp = new Node<T>(value);
+            this->add(temp);
+        }
+        void add(Node<T>* newNode){
+            if(head == NULL){
+                head = newNode;
+                length++;
+                return;
+            }
+            Node<T>* currentNode = head;
+            while(currentNode->getNext() != NULL){
+                currentNode = currentNode->getNext();
+            }
+            currentNode->setNext(newNode);
+            length++;
+            return;
+        }
+        
+        friend ostream& operator<< (ostream& stream, const List& list){
+            Node<T>* currentNode = list.head;
+            stream << "[  ";
+            while(currentNode != NULL){
+                stream << currentNode->getValue() << "  ";
+                currentNode = currentNode->getNext();
+            }
+            stream << "]";
+            return stream;
+        }
 };
 
-int main(){
-	Node<int> n0 = Node<int>(10);
-	Node<int> n1 = Node<int>(15);
-	Node<int> n2 = Node<int>(20);
 
-	List<int> list;
-	list.add(&n0);
-	list.add(&n1);
-	list.add(&n2);
-	list.add(50);
-	cout << list << endl;
 
-	cout << list.search(50) << endl;
+int main(){     
+    List<Studente> list;
+    Studente studente = {"Patrick", "Nicolosi"};
 
-	return 0;
+    list.add(studente);
+
+    cout << list << endl;
+    
+    return 0;
 }
