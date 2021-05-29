005c1ddeddb94d3692a4cb90b4d368341f8193fe
Patrick Nicolosi <patricknicolosi99@gmail.com>
2021-05-29 11:03:28 +0200

    Riscritto esercizione sulle code in maniera più chiara

--- 12-13.cpp
+++ 12-13.cpp
@@ -34,85 +34,79 @@ L'elemento in cima alla lista si chiama top. Come per code anche le pile possono
 Sia il POP che il PUSH avrà sempre tempo costante a prescindere dall'implementazione
 */
 
-//condizione coda piena (h-(t+1))%6 ==0 , se è vero la coda è piena
 #include <iostream>
 using namespace std;
 
-#include <iostream>
-using namespace std;
-
-template<class T>
-class Queue {
+template <class T>
+class Queue{
+	int head, tail = -1;
+	int nElements = 0;
 	int length;
-	int nElem = 0;
-	T *queue;
-	int head = -1, tail = -1;
+	T* queue;
 
 	public:
-	
-	Queue(int length): length(length) {
-		queue = new T[length];
-	}
-	Queue() : Queue(10) {}
-	
-	void enqueue(T value) {
-		if(this->isFull()) 
-			return;
-		if(this->isEmpty()) {
-			head = 0;
+		Queue(int length): length(length) {
+			queue = new T[length];
+		}
+		Queue() : Queue(5) {}
+
+		void enqueue(T value){
+			if(isFull()){
+				return;
+			}
+			if(isEmpty()){
+				head = 0;
+			}
+			tail = ((tail+1) % length); //inserisco questo perchè devo prendere in considerazione l'eventualità in cui la coda sia piena, questa condizione di accesso funziona bene in tutti i casi, sia in una coda piena che in una coda vuota o semi vuota
+			this->queue[tail] = value; 
+			nElements++;
+		}
+		bool dequeue(){
+			if(isEmpty()) return false;
+			head = (head+1) % length;
+			nElements--;
+			return true;
+		}
+		bool isFull(){
+			if((head - (tail+1)) % length == 0) return true;
+			return false;
+		}
+		bool isEmpty(){
+			if(nElements == 0) return true;
+			return false;
+		}
+		int getLength() const{
+			return length;
+		}
+		friend ostream& operator << (ostream& stream, const Queue& queue){
+			for(int i=0; i<queue.length; i++){
+				stream << queue.queue[i] <<  ((i==queue.head) ? " <-- head" : (i == queue.tail) ? " <-- tail" : " ") << endl;
+			}
+			return stream;
 		}
 
-		this->queue[(++tail) % length] = value;
-		tail %= length;
-		nElem++;
-	}
-	T dequeue() {
-		if(this->isEmpty()) {
-			return 0;
-		}
-		//restituire l'elemento in testa e aggiornare head facendo attenzione alla circolarità
-		nElem--;
-		T temp = this->queue[head];
-		head = (head+1) % length;
-		return temp; //this->queue[head++%length];
-	}
-	bool isFull() {
-		return ((head - (tail+1)) % length) == 0;
-	}
-	bool isEmpty() {
-		return nElem == 0; //(head == tail);
-	}
-	int size() const {
-		return this->length - (((this->length - (tail - head)) -1 ) % this->length);
-	}
-	friend ostream& operator<<(ostream& out, const Queue& q) {
-		out << "Coda di lunghezza massima " << q.length << " con " << q.nElem << " inseriti size=" << q.size() << endl;
-		out << "head = " << q.head << " tail = " << q.tail << endl;
-		for(int i=0; i < q.length; i++) 
-			out << "\t" << q.queue[i] << ((i == q.head) ? " <- head" : ((i == q.tail) ? " <- tail" : "")) << endl;
-		return out;
-    }
-	~Queue() {
-		delete [] queue;
-	}
-	
-	
+
 };
 
 
 int main(){
-
-	/*Coda<AAA> coda1; //Per istanziare questa classe ricordiamo di inserire il costruttore di default per la classe AAA perchè abbiamo instaziato nella classe Coda un array coda = new T[lunghezza]
-	cout << coda1 << endl;*/
-
 	Queue<int> queue;
+
+	for(int i=1; i<queue.getLength()+1; i++){
+		queue.enqueue(i);
+		cout << queue << endl;
+		cout << ".............................." << endl << endl;
+	}
+
+	queue.dequeue();
 	cout << queue << endl;
 
-	queue.enqueue(1);
+	cout << ".............................." << endl << endl;
+
+	queue.enqueue(11);
 	cout << queue << endl;
 
-	queue.enqueue(122);
-	cout << queue << endl;
+	cout << ".............................." << endl << endl;
 
 	queue.dequeue();
 	cout << queue << endl;
